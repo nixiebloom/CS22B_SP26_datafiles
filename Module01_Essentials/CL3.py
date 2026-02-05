023585bfe59ffc98ff664c7c0b858088819f4850
nixiebloom <skye.gonigan@gmail.com>
2026-02-05 07:47:53 -0800

    CL3.2

--- Module01_Essentials/CL3.py
+++ Module01_Essentials/CL3.py
@@ -7,10 +7,14 @@
 NL = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 ## Write a list comprehension to collect the diagonal items from matrix NL
 # ** your code **
+diag = [NL[i][i]for i in range(len(NL))]
+print(diag)
 
 ##### CL3.2: Using my_list, create a list of tuples where tuple is (n, n**3) where n is each number in the list.
-my_list = [1, 2, 3, 4, 5]
+dia_list = [1, 2, 3, 4, 5]
 # ** your code **
+power3 =[(n, n**3) for n in dia_list]
+print(power3)
 
 
 ##### CL3.3: Multiple all the values of the items in a dictionary 
